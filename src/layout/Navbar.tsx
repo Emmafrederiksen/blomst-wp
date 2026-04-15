26b94dec9d38202fef79f14527644eeb18b9503a
emil51z8 <emil51z8@edu.zealand.dk>
2026-04-15 12:32:12 +0200

    installed react-lucide + finished navbar styling

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -1,9 +1,21 @@
+"use client";
+
 import Container from "./Container";
 import Link from "next/link";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 import logo from "../../public/images/logo.png";
+import { ShoppingCart } from "lucide-react";
 
 export default function NavBar() {
+  const pathname = usePathname();
+
+  const navLinks = [
+    { name: "Forside", href: "/" },
+    { name: "Shop", href: "/shop" },
+    { name: "Om os", href: "/about" },
+  ];
+
   return (
     <nav className="bg-brand-primary">
       <Container>
@@ -11,31 +23,30 @@ export default function NavBar() {
           {/* Venstre side */}
           <div className="flex items-center gap-3">
             <Image src={logo} alt="Blomst logo" className="w-14 h-auto" />
-            <h1 className="text-brand-white text-xl font-semibold">Blomst</h1>
+            <h1 className="text-brand-white text-h2 font-serif">Blomst</h1>
           </div>
 
           {/* Højre side */}
-          <ul className="flex items-center gap-6 text-brand-white">
-            <li>
-              <Link href="/" className="py-3 block">
-                Forside
-              </Link>
-            </li>
-            <li>
-              <Link href="/shop" className="py-3 block">
-                Shop
-              </Link>
-            </li>
-            <li>
-              <Link href="/" className="py-3 block">
-                Om os
-              </Link>
-            </li>
+          <ul className="flex items-center gap-6 text-brand-white font-sans">
+            {navLinks.map((link) => (
+              <li key={link.href}>
+                <Link
+                  href={link.href}
+                  className={`py-3 block ${
+                    pathname === link.href ? "font-bold" : "font-normal"
+                  }`}
+                >
+                  {link.name}
+                </Link>
+              </li>
+            ))}
+
             <li>
               <Link
                 href="/"
-                className="bg-brand-white text-brand-dark font-sans text-sm font-medium px-6 py-3 rounded-full hover:opacity-90 active:scale-95 transition-all duration-200 whitespace-nowrap"
+                className="flex items-center gap-2 bg-brand-white text-brand-dark font-medium px-6 py-3 rounded-full hover:opacity-90 active:scale-95 transition-all duration-200 whitespace-nowrap"
               >
+                <ShoppingCart className="w-6 h-6" />
                 Kurv
               </Link>
             </li>
